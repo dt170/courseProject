da873976a7ad1e3b8a2e55f8734412e3e0534e98
DT <dvirto@mta.ac.il>
2021-10-11 16:43:41 +0300

    📝

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -7,7 +7,10 @@ import {Recipe} from '../recipe.model'
 })
 export class RecipeListComponent implements OnInit {
   recipes:Recipe[]=[
-    new Recipe("testRecipe","This is a test",
+    new Recipe("A Test Recipe1","This is a test",
+    "https://www.simplyrecipes.com/thmb/OCi18J2V8OeKDFV3FxoeKvgq74E=/1423x1067/smart/filters:no_upscale()/__opt__aboutcom__coeus__resources__content_migration__simply_recipes__uploads__2012__07__grilled-sweet-potatoes-horiz-a-1600-7c8292daa98e4020b447f0dc97a45cb7.jpg"
+    ),
+    new Recipe("A Test Recipe2","This is a test",
     "https://www.simplyrecipes.com/thmb/OCi18J2V8OeKDFV3FxoeKvgq74E=/1423x1067/smart/filters:no_upscale()/__opt__aboutcom__coeus__resources__content_migration__simply_recipes__uploads__2012__07__grilled-sweet-potatoes-horiz-a-1600-7c8292daa98e4020b447f0dc97a45cb7.jpg"
     ),
   ];
